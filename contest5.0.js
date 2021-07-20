79461170b680377ed0a9b8fc2633d92db90b58ff
Polina Shneider <beatrixlinkoln@gmail.com>
2021-07-21 00:52:25 +0300

    Tasks 20.07

--- contest5.0.js
+++ contest5.0.js
@@ -2811,3 +2811,46 @@ const lowestCommonAncestor = (root, p, q) => {
     }
     return null;
 };
+
+/**
+ * @param {number[]} nums
+ */
+var Solution = function (nums) {
+    this.nums = nums || []
+};
+
+/**
+ * Resets the array to its original configuration and return it.
+ * @return {number[]}
+ */
+Solution.prototype.reset = function () {
+    return this.nums
+};
+
+/**
+ * Returns a random shuffling of the array.
+ * @return {number[]}
+ */
+Solution.prototype.shuffle = function () {
+    // create a copy of this.nums, shuffle it, and return it
+    const shuffled = this.nums.slice();
+    const n = shuffled.length;
+    const swap = (arr, i, j) => {
+        let tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    // swap elements with random elements
+    for (let i = 0; i < n; i++)
+        swap(shuffled, i, Math.floor(Math.random() * n));
+
+    return shuffled;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.reset()
+ * var param_2 = obj.shuffle()
+ */
