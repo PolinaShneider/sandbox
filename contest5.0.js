870d4e1faab2f3e7566810d274d208bfe45898a2
Polina Shneider <beatrixlinkoln@gmail.com>
2021-07-26 01:57:24 +0300

    Tasks 25.07

--- contest5.0.js
+++ contest5.0.js
@@ -2973,3 +2973,36 @@ function alienOrder(words) {
     //Return the return string if the length is the same as the count of unique characters, else return empty string
     return uniqueCharString.length === uniqueCharCount.size ? uniqueCharString : "";
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var findIntegers = function (n) {
+    let dp = len => {
+        if (len < 0)
+            return 0;
+        if (!len)
+            return 1;
+        let _0x = 1; // number of accepted combination when '1' is first
+        let _1x = 1; // number of accepted combination when '0' is first
+        while (--len)
+            [_0x, _1x] = [_0x + _1x, _0x];
+        return _0x + _1x;
+    };
+    let binary = n.toString(2);
+    let count = 0;
+    let is_prev_one = false;
+    for (let i = 0; i < binary.length; i++) {
+        if (binary[i] === '0') {
+            is_prev_one = false;
+            continue;
+        }
+        count += dp(binary.length - i - 1);
+        if (is_prev_one)
+            return count;
+        is_prev_one = true;
+    }
+    return count + 1;
+};
+
