4a479663ce9ec15753c371e43659f1dfae8796ba
Polina Shneider <beatrixlinkoln@gmail.com>
2021-08-03 01:49:41 +0300

    Tasks 02.08

--- contest5.0.js
+++ contest5.0.js
@@ -3263,3 +3263,18 @@ var minCostToSupplyWater = function (n, wells, pipes) {
     }
     return res
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+};
