3187c40fceee1b83c7bc798c4a280faad498764b
Polina Shneider <beatrixlinkoln@gmail.com>
2021-08-14 00:00:17 +0300

    Tasks 13.08

--- contest5.0.js
+++ contest5.0.js
@@ -3550,3 +3550,30 @@ var groupAnagrams = function (strs) {
     return Object.values(map)
 };
 
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function (matrix) {
+    arr = [];
+    for (let row = 0; row < matrix.length; row += 1) {
+        for (let col = 0; col < matrix[row].length; col += 1) {
+            if (matrix[row][col] === 0) arr.push([row, col])
+        }
+    }
+
+    for (let change of arr) {
+        helper(change)
+    }
+
+    function helper(change) {
+        let [row, col] = change
+        for (let i = 0; i < matrix.length; i += 1) {
+            matrix[i][col] = 0
+        }
+        for (let j = 0; j < matrix[0].length; j += 1) {
+            matrix[row][j] = 0
+        }
+    }
+};
+
