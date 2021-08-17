966e9234ffdbe0aaad090e224c983280f220b21d
Polina Shneider <beatrixlinkoln@gmail.com>
2021-08-18 00:08:54 +0300

    Tasks 17.08

--- contest5.0.js
+++ contest5.0.js
@@ -3678,3 +3678,21 @@ NumArray.prototype.sumRange = function (i, j) {
  * var obj = new NumArray(nums)
  * var param_1 = obj.sumRange(i,j)
  */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function (root, val = -Infinity) {
+    if (!root) return 0;
+    const max = Math.max(root.val, val);
+    return goodNodes(root.left, max) + goodNodes(root.right, max) + (root.val >= val ? 1 : 0);
+};
