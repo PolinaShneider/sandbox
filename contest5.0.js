847ce5232b9325a43da75da89b8cf03b883124c5
Polina Shneider <beatrixlinkoln@gmail.com>
2021-08-25 00:03:45 +0300

    Tasks 24.08

--- contest5.0.js
+++ contest5.0.js
@@ -3930,3 +3930,28 @@ var validTree = function (n, edges) {
 
     return seen.size === n;
 };
+
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var complexNumberMultiply = function (num1, num2) {
+    /*
+    Approach: By doing maths we know that
+    (a+b)*(c+d)
+    => ac + (ad)i + (bc)i + -(db)
+    => ac - db + (ad+bc)i
+    Now we just need to find a,b,c,d and then calculate ac - db + (ad+bc)i
+    */
+    let num1Parts = num1.split("+");
+    let num2Parts = num2.split("+");
+    let a = parseInt(num1Parts[0]);
+    let b = parseInt(num1Parts[1]);
+    let c = parseInt(num2Parts[0]);
+    let d = parseInt(num2Parts[1]);
+    let realPart = a * c - b * d;
+    let imaginaryPart = a * d + b * c;
+    let ans = "" + realPart + "+" + imaginaryPart + "i";
+    return ans;
+};
